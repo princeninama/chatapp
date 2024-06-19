b90da3ec5875f671658d5027512ebc458c32cf91
princeninama <princeninama254@gmail.com>
2024-06-19 15:39:03 +0530

    working fine

--- Backend/Middleware/protect.js
+++ Backend/Middleware/protect.js
@@ -4,7 +4,7 @@ import User from '../schema/user.js';
 const protect = async (req, res, next) => {
   try {
     const token = req.cookies.jwt;
-    
+
     if (!token) {
       return res.status(401).json({ error: 'No token found' });
     }
